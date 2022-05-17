a31a8709c6b4f21c5a45408bb3b3c571ea9822fc
Blextor <78981647+Blextor@users.noreply.github.com>
2022-05-17 15:04:42 +0200

    Sikerült a Maven-nel tesztelésre bírni az mvn clean test paranccsal
    Kéne működnie a GitHub automata kiértékelőjével is

--- src/test/java/core/RunCucumberTest.java
+++ src/test/java/core/RunCucumberTest.java
@@ -1,4 +1,4 @@
-/*
+
 package core;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
@@ -6,8 +6,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-//import static core.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-//import static core.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+//import static junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+//import static junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -15,24 +15,25 @@ import org.junit.platform.suite.api.Suite;
 //@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 //@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "core")
 public class RunCucumberTest {
-    int i= 0;
-    //System.out.printLine("hapci");
+}
+
+
+
+
+/*
+package core;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+public class RunCucumberTest {
+
 }
 
  */
-
-
-
-package core;
-
-        import io.cucumber.junit.Cucumber;
-        import io.cucumber.junit.CucumberOptions;
-        import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions()
-public class RunCucumberTest {
-}
 
 
 
