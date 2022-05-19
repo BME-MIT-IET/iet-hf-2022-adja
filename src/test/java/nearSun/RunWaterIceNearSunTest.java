6d16474366dff139370ab78d0dc948041201e12f
Vida Péter <vida.peter1127@gmail.com>
2022-05-19 23:07:51 +0200

    BDD test uniq behaviors near sun water ice, uran

---
+++ src/test/java/nearSun/RunWaterIceNearSunTest.java
@@ -0,0 +1,19 @@
+package nearSun;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("nearSun")
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "nearSun")
+public class RunWaterIceNearSunTest {
+}
